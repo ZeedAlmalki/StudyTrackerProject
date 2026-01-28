cc97c4747e5f9b41ad7fa67a5a819e82d202eeb5
ZeedAlmalki <zeedalmaki@gmail.com>
2026-01-28 19:43:30 +0300

    Add project files.

---
+++ StudyTracker/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace StudyTracker
+{
+    partial class StudyTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudyTracker));
+            this.TimerStudyTracker = new System.Windows.Forms.Timer(this.components);
+            this.DetailsAboutTimer = new System.Windows.Forms.MenuStrip();
+            this.timerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbTimer = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudyFor = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.maskedTextBoxTimer = new System.Windows.Forms.MaskedTextBox();
+            this.btnOffTimer = new System.Windows.Forms.Button();
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listViewAboutStatus = new System.Windows.Forms.ListView();
+            this.ElapsedTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BasedTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Reason = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Start = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.End = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DetailsAboutTimer.SuspendLayout();
+            this.tbTimer.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TimerStudyTracker
+            // 
+            this.TimerStudyTracker.Interval = 1000;
+            this.TimerStudyTracker.Tick += new System.EventHandler(this.TimerStudyTracker_Tick);
+            // 
+            // DetailsAboutTimer
+            // 
+            this.DetailsAboutTimer.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timerToolStripMenuItem});
+            this.DetailsAboutTimer.Location = new System.Drawing.Point(0, 0);
+            this.DetailsAboutTimer.Name = "DetailsAboutTimer";
+            this.DetailsAboutTimer.Size = new System.Drawing.Size(407, 24);
+            this.DetailsAboutTimer.TabIndex = 9;
+            this.DetailsAboutTimer.Text = "menuStrip1";
+            // 
+            // timerToolStripMenuItem
+            // 
+            this.timerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.timerToolStripMenuItem.Name = "timerToolStripMenuItem";
+            this.timerToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.timerToolStripMenuItem.Text = "Timer";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save Progress";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // tbTimer
+            // 
+            this.tbTimer.Controls.Add(this.tabPage1);
+            this.tbTimer.Controls.Add(this.tabPage2);
+            this.tbTimer.Location = new System.Drawing.Point(0, 27);
+            this.tbTimer.Name = "tbTimer";
+            this.tbTimer.SelectedIndex = 0;
+            this.tbTimer.Size = new System.Drawing.Size(1125, 726);
+            this.tbTimer.TabIndex = 10;
+            this.tbTimer.Click += new System.EventHandler(this.tbTimer_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtStudyFor);
+            this.tabPage1.Controls.Add(this.lblTimer);
+            this.tabPage1.Controls.Add(this.maskedTextBoxTimer);
+            this.tabPage1.Controls.Add(this.btnOffTimer);
+            this.tabPage1.Controls.Add(this.btnStartTimer);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1117, 700);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "About Timer";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Study Goal";
+            // 
+            // txtStudyFor
+            // 
+            this.txtStudyFor.Location = new System.Drawing.Point(1, 137);
+            this.txtStudyFor.Name = "txtStudyFor";
+            this.txtStudyFor.Size = new System.Drawing.Size(208, 20);
+            this.txtStudyFor.TabIndex = 14;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.ForeColor = System.Drawing.Color.Black;
+            this.lblTimer.Location = new System.Drawing.Point(165, 236);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 13);
+            this.lblTimer.TabIndex = 11;
+            // 
+            // maskedTextBoxTimer
+            // 
+            this.maskedTextBoxTimer.Location = new System.Drawing.Point(1, 54);
+            this.maskedTextBoxTimer.Mask = "0:00:00";
+            this.maskedTextBoxTimer.Name = "maskedTextBoxTimer";
+            this.maskedTextBoxTimer.Size = new System.Drawing.Size(196, 20);
+            this.maskedTextBoxTimer.TabIndex = 13;
+            // 
+            // btnOffTimer
+            // 
+            this.btnOffTimer.Location = new System.Drawing.Point(95, 80);
+            this.btnOffTimer.Name = "btnOffTimer";
+            this.btnOffTimer.Size = new System.Drawing.Size(107, 23);
+            this.btnOffTimer.TabIndex = 12;
+            this.btnOffTimer.Text = "Off";
+            this.btnOffTimer.UseVisualStyleBackColor = true;
+            this.btnOffTimer.Click += new System.EventHandler(this.btnOffTimer_Click);
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(1, 80);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(105, 23);
+            this.btnStartTimer.TabIndex = 10;
+            this.btnStartTimer.Text = "Start";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            this.btnStartTimer.Click += new System.EventHandler(this.btnStartTimer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Set a reminder notification";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listViewAboutStatus);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1117, 700);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About Status";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listViewAboutStatus
+            // 
+            this.listViewAboutStatus.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ElapsedTime,
+            this.BasedTime,
+            this.Reason,
+            this.Start,
+            this.End});
+            this.listViewAboutStatus.GridLines = true;
+            this.listViewAboutStatus.HideSelection = false;
+            this.listViewAboutStatus.Location = new System.Drawing.Point(0, 0);
+            this.listViewAboutStatus.Name = "listViewAboutStatus";
+            this.listViewAboutStatus.Size = new System.Drawing.Size(1125, 726);
+            this.listViewAboutStatus.TabIndex = 0;
+            this.listViewAboutStatus.UseCompatibleStateImageBehavior = false;
+            this.listViewAboutStatus.View = System.Windows.Forms.View.Details;
+            // 
+            // ElapsedTime
+            // 
+            this.ElapsedTime.Text = "ElapsedTime";
+            this.ElapsedTime.Width = 200;
+            // 
+            // BasedTime
+            // 
+            this.BasedTime.Text = "BasedTime";
+            this.BasedTime.Width = 200;
+            // 
+            // Reason
+            // 
+            this.Reason.Text = "Reason";
+            this.Reason.Width = 200;
+            // 
+            // Start
+            // 
+            this.Start.Text = "Start";
+            this.Start.Width = 200;
+            // 
+            // End
+            // 
+            this.End.Text = "End";
+            this.End.Width = 200;
+            // 
+            // StudyTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 415);
+            this.Controls.Add(this.tbTimer);
+            this.Controls.Add(this.DetailsAboutTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.DetailsAboutTimer;
+            this.MaximizeBox = false;
+            this.Name = "StudyTracker";
+            this.Text = "StudyTracker";
+            this.DetailsAboutTimer.ResumeLayout(false);
+            this.DetailsAboutTimer.PerformLayout();
+            this.tbTimer.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer TimerStudyTracker;
+        private System.Windows.Forms.MenuStrip DetailsAboutTimer;
+        private System.Windows.Forms.ToolStripMenuItem timerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TabControl tbTimer;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudyFor;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTimer;
+        private System.Windows.Forms.Button btnOffTimer;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStartTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listViewAboutStatus;
+        private System.Windows.Forms.ColumnHeader ElapsedTime;
+        private System.Windows.Forms.ColumnHeader BasedTime;
+        private System.Windows.Forms.ColumnHeader Reason;
+        private System.Windows.Forms.ColumnHeader Start;
+        private System.Windows.Forms.ColumnHeader End;
+    }
+}
+
---
+++ StudyTracker/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyTracker
+{
+    public partial class StudyTracker : Form
+    {
+        public StudyTracker()
+        {
+            InitializeComponent();
+        }
+        uint Time = 0;
+        int Seconds = 0;
+        int Minutes = 0;
+        int Hours = 0;
+        string TimeOutOf = "";
+        string StudyingFor = "";
+        bool isOff;
+        bool HasBeenStarted = false;
+
+        bool isValid()
+        {
+
+            if (!maskedTextBoxTimer.MaskCompleted)
+            {
+                MessageBox.Show("Please Fill The Timer");
+                return false;
+            }
+
+            if (txtStudyFor.Text == string.Empty)
+            {
+                MessageBox.Show("Please Enter The Thing You Study For");
+                return false;
+            }
+
+            return true;
+        }
+
+        void SetTime()
+        {
+
+            Hours = Convert.ToInt32(maskedTextBoxTimer.Text.Substring(0, 1));
+            Minutes = Convert.ToInt32(maskedTextBoxTimer.Text.Substring(2, 2));
+            Seconds = Convert.ToInt32(maskedTextBoxTimer.Text.Substring(5, 2));
+        }
+
+        string getStudyingFor()
+        {
+            return txtStudyFor.Text;
+        }
+
+        void TimeCorrection(bool isType1 = true)
+        {
+            if (isType1)
+            { 
+            string stSecond = Convert.ToString(Seconds);
+            if (Seconds <= 9)
+            {
+
+                stSecond = "0" + Seconds;
+            }
+
+            string stMinute = Convert.ToString(Minutes);
+            if (Minutes <= 9)
+            {
+
+                stMinute = "0" + stMinute;
+            }
+
+            TimeOutOf = (Hours + ":" + stMinute + ":" + stSecond);
+
+            }
+            else
+            {
+                string stSecondsOfTimer = SecondsOfTimer.ToString();
+                string stMinuteOfTimer = MinutesOfTimer.ToString();
+                if (SecondsOfTimer <= 9)
+                {
+
+                    stSecondsOfTimer = "0" + stSecondsOfTimer;
+                }
+
+                if (MinutesOfTimer <= 9)
+                {
+
+                    stMinuteOfTimer = "0" + stMinuteOfTimer;
+                }
+
+                lblTimer.Text = (HoursOfTimer + ":" + stMinuteOfTimer + ":" + stSecondsOfTimer);
+            }
+        }
+
+        void setSetteingsOfIsStart()
+        {
+            isOff = false;
+            TimerStudyTracker.Enabled = true;
+            HasBeenStarted = true;
+        }
+
+        private void btnStartTimer_Click(object sender, EventArgs e)
+        {
+            //= Convert.ToInt32(txtBoxTimer.Text);
+
+            if (!isValid())
+                return;
+
+            StudyingFor = getStudyingFor();
+            SetTime();
+            TimeCorrection();
+            StartAt = FullDate(DateTime.Now);
+            setSetteingsOfIsStart();
+        }
+
+        string StartAt = "";
+        string EndAt = "";
+        int SecondsOfTimer = 0;
+        int MinutesOfTimer = 0;
+        int HoursOfTimer = 0;
+
+        bool CheckIfTimerCompleted()
+        {
+            if (SecondsOfTimer == Convert.ToInt32(Seconds) && (MinutesOfTimer == Convert.ToInt32(Minutes) && (HoursOfTimer == Convert.ToInt32(Hours))))
+            {
+                TimerStudyTracker.Enabled = false;
+                MessageBox.Show("Completed");
+                SaveData();
+                return true;
+            }
+
+            return false;
+        }
+
+        void CounterOfTimer()
+        {
+            SecondsOfTimer++;
+
+            if (SecondsOfTimer == 60)
+            {
+                MinutesOfTimer++;
+                SecondsOfTimer = 0;
+            }
+
+            if (MinutesOfTimer == 60)
+            {
+                HoursOfTimer++;
+                MinutesOfTimer = 0;
+                SecondsOfTimer = 0;
+            }
+        }
+
+        bool CheckIfReachTheMaximumTime()
+        {
+            if (HoursOfTimer == 9 && MinutesOfTimer == 59 && SecondsOfTimer == 59)
+            {
+                TimerStudyTracker.Enabled = false;
+                MessageBox.Show("You Completed 9 Hours and 59 Minutes And 59 Seconds, The maximum of the Program");
+                SaveData();
+                return true;
+            }
+            return false;
+        }
+        private void TimerStudyTracker_Tick(object sender, EventArgs e)
+        {
+            if (CheckIfTimerCompleted())
+                return;
+
+            CounterOfTimer();
+
+            if (CheckIfReachTheMaximumTime())
+                return;
+
+            TimeCorrection(false);
+
+        }
+
+        void setSetteingsOfIsOff()
+        {
+            TimerStudyTracker.Enabled = false;
+            isOff = true;
+        }
+
+        private void btnOffTimer_Click(object sender, EventArgs e)
+        {
+            if (isOff == true)
+            {
+                MessageBox.Show("Timer Is Stopped");
+                return;
+            }
+
+            setSetteingsOfIsOff();
+            MessageBox.Show("Timer Has Been Stopped");
+            return;
+        }
+
+        private void showStatusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbTimer.SelectedTab = tabPage2;
+            StatusSize();
+        }
+
+        static string FilePath = @"D:\Course 14 C#\StudyTracker\StudyTimerInfo.txt";
+
+        ListViewItem AddItemsByParts(string[] parts)
+        {
+            ListViewItem item = new ListViewItem(parts[0].ToString());
+            item.SubItems.Add(parts[1].ToString());
+            item.SubItems.Add(parts[2].ToString());
+            item.SubItems.Add(parts[3].ToString());
+            item.SubItems.Add(parts[4].ToString());
+
+            return item;
+        }
+
+        void DefaultSize()
+        {
+            this.Size = new Size(423, 454);
+        }
+        
+        void StatusSize()
+        {
+            this.Size = new Size(1125, 726);
+        }
+
+        private void tbTimer_Click(object sender, EventArgs e)
+        {
+            if (tbTimer.SelectedTab == tabPage2)
+            {
+                listViewAboutStatus.Items.Clear();
+                string[] Lines = new string[] { };
+                StatusSize();
+
+                Lines = File.ReadAllLines(FilePath);
+
+                foreach(string Line in Lines)
+                {
+                    string[] parts = Line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+                    listViewAboutStatus.Items.Add(AddItemsByParts(parts));
+                }
+            }
+            else if (tbTimer.SelectedTab == tabPage1)
+            {
+                DefaultSize();
+            }
+
+        }
+
+
+        string FullDate(DateTime dt)
+        {
+
+            return dt.Year.ToString() + "/" + dt.Month.ToString() + "/" + dt.Day.ToString() + "/" + dt.Hour.ToString() + ":" + dt.Minute.ToString();
+        }
+
+
+        bool IsStart()
+        {
+            if (!(HasBeenStarted))
+            {
+                MessageBox.Show("You Haven't Started The Timer Yet!", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return false;
+            }
+            return true;
+        }
+
+        string AllDataText()
+        {
+            return lblTimer.Text + "#//#" + TimeOutOf + "#//#" + StudyingFor + "#//#" + StartAt + "#//#" + EndAt + Environment.NewLine;
+        }
+
+        void SaveData()
+        {
+            File.AppendAllText(FilePath, AllDataText());
+            this.Close();
+        }
+
+
+        void SaveDataAfterAsk()
+        {
+            if (MessageBox.Show("Are You Sure ? You Will Close The Form", "Close The Form", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                SaveData();
+            }
+
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!IsStart())
+                return;
+
+            EndAt = FullDate(DateTime.Now);
+
+            SaveDataAfterAsk();
+          
+        }
+    }
+}
